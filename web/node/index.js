cb05a737377e1056e6e38d45d526f23058d1d98b
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-08-26 20:24:27 -0300

    feat: add config reading environment variables and correct the request of data from database

--- web/node/index.js
+++ web/node/index.js
@@ -5,40 +5,45 @@ const app = express();
 const port = 3000;
 
 const config = {
-  host: "db-mysql",
-  user: "root",
-  password: "root",
-  database: "challengedb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 const connection = mysql.createConnection(config);
 connection.connect((err) => {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error("Error connecting: " + err.stack);
     return;
   }
   console.log("Connected as id " + connection.threadId);
-});
 
-const stmt = `INSERT INTO people (name) VALUES ('Higor Cruz');`;
-connection.query(stmt);
-let names;
-const query = "SELECT name FROM people";
-connection.query(query, (err, results) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  names = results.map((row) => row.name);
+  const stmt = `INSERT INTO people (name) VALUES ('Higor Cruz');`;
+  connection.query(stmt, (err, results) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Default data inserted successfully!");
+  });
 });
-connection.end();
 
 app.get("/", (req, res) => {
-  const htmlResult =
-    `<h1>Full Cycle Rocks!</h1>
-    <ul>` +
-    names.map((name) => `<li>${name}</li>`).join("") +
-    `</ul>`;
-  res.send(htmlResult);
+  const stmt = "SELECT name FROM people;";
+  connection.query(stmt, (err, results) => {
+    if (err) {
+      console.error("Error retrieving data: " + err.stack);
+      return;
+    }
+    const names = results.map((row) => row.name);
+    const html = `
+      <h1>Full Cycle Rocks!</h1>
+      <ul>
+        ${names.map((name) => `<li>${name}</li>`).join("")}
+      </ul>
+    `;
+    res.send(html);
+  });
 });
 
 app.listen(port, () => {
